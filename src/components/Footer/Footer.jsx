2a8105b0d14c8d97fae12d4da122e784e0aeb828
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2024-04-12 13:31:01 -0300

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,11 @@
 // Footer.jsx
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importar Link desde React Router
 import style from './Footer.module.css';
 
 const Footer = () => {
   return (
     <div className={style.footer}>
       <p>© 2023 Mi Website \\  Falciglio Nicolas </p>
-      {/* Enlace al componente FormularioCargaDatos */}
-      <Link to="/admin">
-        <button>BOTON</button>
-      </Link>
     </div>
   );
 };
