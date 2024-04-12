9d8546bd550a5df378e5aeebe846b885f29e394d
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2024-04-12 12:00:30 -0300

    rout

--- src/routers/RouterPrincipal.jsx
+++ src/routers/RouterPrincipal.jsx
@@ -1,10 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import ItemListContainer from "../components/ItemListCointainer/ItemListContainer";
 import ItemDetailContainer from "../components/ItemDetailContainer/ItemDetailContainer";
-import NavBar from "../components/NavBar/NavBar";
 import { CartProvider } from "../context/CartContext";
-import Cart from "../components/Cart/Cart";
-import Order from "../components/Order/Order.jsx";
 import FormularioCargaDatos from "../components/ProductComponent/FormularioCargaDatos.jsx"
 
 const RouterPrincipal = () => {
@@ -15,8 +12,6 @@ const RouterPrincipal = () => {
             <Route path="/" element={<ItemListContainer />} />
             <Route path="/category/:categoryId" element={<ItemListContainer />}></Route>
             <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<Cart/>} />
-            <Route path="/checkout" element={<Order/>} />
             <Route path="/admin" element={<FormularioCargaDatos/>}/>
           </Routes>
         </CartProvider>
