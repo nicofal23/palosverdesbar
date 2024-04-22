15fbd1a2fc5331136feddfd6b0dd8d65e7c6e06b
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2024-04-22 10:21:07 -0300

    ..

--- src/components/ModificarProductos/ModificarProduct.jsx
+++ src/components/ModificarProductos/ModificarProduct.jsx
@@ -4,7 +4,6 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import styles from '../ModificarProductos/ModificarProductos.module.css';
 import SelectCategory from '../SelectProduct/SelectProduct';
-import ProductSearch from '../ProductSearch/ProductSearch';
 
 const ModificarProductos = () => {
     const [productos, setProductos] = useState([]);
@@ -15,18 +14,18 @@ const ModificarProductos = () => {
         const fetchProductos = async () => {
             try {
                 let productosQuery = collection(db, 'productos');
-
+    
                 // Si hay una categoría seleccionada, agregar filtro por categoría a la consulta
                 if (categoriaSeleccionada) {
                     productosQuery = query(productosQuery, where("category", "==", categoriaSeleccionada));
                 }
-
+    
                 // Si hay un término de búsqueda, agregar filtro por nombre a la consulta
                 if (searchTerm) {
                     const searchTermLower = searchTerm.toLowerCase();
-                    productosQuery = query(productosQuery, where("nombre","==", searchTermLower));
+                    productosQuery = query(productosQuery, where("nombre", "contains", searchTermLower));
                 }
-
+    
                 const productosSnapshot = await getDocs(productosQuery);
                 const productosData = productosSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
                 console.log("Productos obtenidos:", productosData); // Agregar este console.log para depurar
@@ -37,6 +36,7 @@ const ModificarProductos = () => {
         };
         fetchProductos();
     }, [categoriaSeleccionada, searchTerm]);
+    
 
     // Función para manejar el cambio de categoría seleccionada
     const handleCategoriaChange = (event) => {
@@ -56,7 +56,15 @@ const ModificarProductos = () => {
             <h2>Lista de Productos</h2>
             <div className={styles.filtro}>
                 <SelectCategory value={categoriaSeleccionada} onChange={handleCategoriaChange} />
-                <ProductSearch onSearch={handleSearch} />
+                <div>
+                    <label htmlFor="search">Buscar por nombre: </label>
+                    <input
+                        type="text"
+                        id="search"
+                        value={searchTerm}
+                        onChange={(e) => handleSearch(e.target.value)}
+                    />
+                </div>
             </div>
             <ul className={styles.productList}>
                 {productos.map(producto => (
@@ -70,9 +78,9 @@ const ModificarProductos = () => {
                                     {producto.descripcion}
                                 </p>
                             </div>
-                            <section className={styles.productDetails}> 
+                            <section className={styles.productDetails}>
                                 <p className={styles.productPrice}>
-                                    ${producto.precio} 
+                                    ${producto.precio}
                                 </p>
                                 <p className={styles.productStock}>
                                     Stock: {producto.stock}
